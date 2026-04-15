5f1ece1b2621d1727458e1ea3a547c3c7e60b363
igorrochap <igor.systemof@gmail.com>
2026-04-15 12:32:34 -0300

    feat(docs): adiciona Scramble e remove rota padrão
    Instala dedoc/scramble para gerar documentação OpenAPI automaticamente.
    Remove endpoint /user padrão do Laravel Sanctum para limpeza inicial do
    arquivo de rotas.

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,4 @@ use App\Http\Controllers\SignupController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/signup', SignupController::class);
