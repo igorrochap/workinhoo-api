a39143c68b4a1df5d68366a968f11707026287b5
Eduardo Pereira <doardoperera@gmail.com>
2026-05-01 11:46:39 -0300

    feat: criação de model de portfolio

---
+++ app/Models/Prestador/Portfolio.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models\Prestador;
+
+use App\Support\ValueObjects\UUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Portfolio extends Model
+{
+    use HasFactory;
+
+    protected $table = 'portfolios_prestadores';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id',
+        'uuid',
+        'prestador_id',
+        'descricao',
+        'midia_path',
+    ];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+        static::creating(function (Portfolio $portfolio) {
+            $portfolio->uuid = UUID::cria()->recupera();
+        });
+    }
+
+    public function prestador(): BelongsTo
+    {
+        return $this->belongsTo(Prestador::class, 'prestador_id');
+    }
+}
