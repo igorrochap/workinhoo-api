04fadca633f10f75da1390aa018b7ec7daef5f92
Eduardo Pereira <doardoperera@gmail.com>
2026-05-02 12:11:42 -0300

    refactor: adiciona anotação de propriedades para PHPStan no model Portfolio

--- app/Models/Prestador/Portfolio.php
+++ app/Models/Prestador/Portfolio.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+// propriedade para phpstan
+/**
+ * @property \App\Models\Prestador\Prestador $prestador
+ */
 class Portfolio extends Model
 {
     use HasFactory;
