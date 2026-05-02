30476ca1534202ca049ba5cbf9e1da191a06ab1f
Eduardo Pereira <doardoperera@gmail.com>
2026-05-02 12:09:55 -0300

    refactor: retira métodos criados automaticamente da policy de portfolio

--- app/Policies/PortfolioPolicy.php
+++ app/Policies/PortfolioPolicy.php
@@ -7,31 +7,7 @@ use App\Models\Usuario\Usuario;
 
 class PortfolioPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(Usuario $usuario): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(Usuario $usuario, Portfolio $portfolio): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(Usuario $usuario): bool
-    {
-        return false;
-    }
-
-    /**
+       /**
      * Determine whether the user can update the model.
      */
     public function update(Usuario $usuario, Portfolio $portfolio): bool
@@ -45,21 +21,5 @@ class PortfolioPolicy
     public function delete(Usuario $usuario, Portfolio $portfolio): bool
     {
         return $portfolio->prestador->usuario_id === $usuario->id;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(Usuario $usuario, Portfolio $portfolio): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(Usuario $usuario, Portfolio $portfolio): bool
-    {
-        return false;
     }
 }
