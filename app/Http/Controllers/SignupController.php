8c7314122be384bde89f53e2dc3acc79f7be5434
quokequack <roymustangtaizo@gmail.com>
2026-05-13 12:28:56 -0300

    fix: remocao do envio de email no signup

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -4,30 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Actions\Signup;
 use App\DTO\SignupDTO;
-use App\Events\VerificarEmailEvent;
 use App\Http\Requests\SignupRequest;
-use App\Services\Auth\VerificarEmail\VerificacaoEmailTokenService;
 use Illuminate\Http\JsonResponse;
 
 class SignupController extends Controller
 {
-    public function __invoke(
-        SignupRequest $request,
-        Signup $signup,
-        VerificacaoEmailTokenService $verificacaoEmail,
-    ): JsonResponse {
-        $usuario = $signup->executa(SignupDTO::porRequest($request));
-
-        if ($usuario) {
-            $response = $verificacaoEmail->enviaCodigo($usuario->email);
-            if ($response) {
-                VerificarEmailEvent::dispatch(
-                    $response['email'],
-                    $response['nome'],
-                    $response['codigo'],
-                );
-            }
-        }
+    public function __invoke(SignupRequest $request, Signup $signup): JsonResponse
+    {
+        $signup->executa(SignupDTO::porRequest($request));
 
         return $this->criado(['message' => 'Caso seu cadastro seja válido, você receberá um email de confirmação']);
     }
